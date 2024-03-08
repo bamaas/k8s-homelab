c3dfae34c6fc5b72e6065f7564841ceba9545c2a
Bas Maas <beheerder@MacBook-Pro-van-Jeffrey.local>
2024-03-08 16:56:36 +0100

    alertmanager integrated with gotify via alertmanager gotify bridge

--- apps/longhorn/create-basic-auth-secret.sh
+++ apps/longhorn/create-basic-auth-secret.sh
@@ -3,5 +3,5 @@
 # https://github.com/longhorn/longhorn/blob/v0.8.0/docs/longhorn-ingress.md
 user=$1; password=$2; output=$3
 echo "${user}:$(openssl passwd -stdin -apr1 <<< ${password})" >> auth
-kubectl create secret generic basic-auth --dry-run=client -o yaml --from-file=auth > ${output}
+kubectl create secret generic basic-auth --dry-run=client -o yaml --from-file=auth > "${output}"
 rm auth
